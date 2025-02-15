75823b3bd8003aaffaf4b6ed81b2e2b2f095b6db
fasuyaaaPNG <dhavinf.a@gmail.com>
2025-02-15 22:56:00 +0700

    add nav::navbar

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const profileTrainer = document.querySelector('.profileTrainer');
-//     let direction = 1; // 1 untuk scroll ke kanan, -1 untuk scroll ke kiri
-//     const step = 1;    // gunakan 1 pixel per interval
-//     const intervalDelay = 10; // delay dalam milidetik
-  
-//     function autoScroll() {
-//       // Debug: tampilkan nilai scroll saat ini
-//       console.log(
-//         'scrollLeft:', profileTrainer.scrollLeft, 
-//         'clientWidth:', profileTrainer.clientWidth, 
-//         'scrollWidth:', profileTrainer.scrollWidth
-//       );
-      
-//       // Cek jika sudah mencapai ujung kanan dengan toleransi 2 pixel
-//       if (profileTrainer.scrollLeft + profileTrainer.clientWidth >= profileTrainer.scrollWidth - 2) {
-//         direction = -1;
-//       }
-//       // Cek jika sudah mencapai ujung kiri
-//       if (profileTrainer.scrollLeft <= 0) {
-//         direction = 1;
-//       }
-//       // Geser scroll berdasarkan arah dan step
-//       profileTrainer.scrollLeft += direction * step;
-//     }
-  
-//     setInterval(autoScroll, intervalDelay);
-//   });
-  
+function myFunction(x) {
+    x.classList.toggle("change");
+    document.querySelector(".navbar").classList.toggle("navbar-enabled");
+}
